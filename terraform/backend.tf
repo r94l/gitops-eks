def7d87aeccd616d57677841d4a276402fbbf552
Raheem <raheem.shonubi@gmail.com>
2026-04-13 23:33:07 +0100

    Clear backend configuration in backend.tf
    Updated backend configuration to use empty values.

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "conduit-terraform-state-2026"
-    key            = "production/terraform.tfstate"
-    region         = "eu-north-1"
-    dynamodb_table = "fullstack-terraform-locks"
-    encrypt        = true
-  }
-}
